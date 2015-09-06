09374339f3140f494881c05568ee9c3eb1ab9344
Federico Krum <federico@krum.com.ar>
2015-09-06 14:33:55 -0300

    Update ts3_backup.sh

--- ts3_backup.sh
+++ ts3_backup.sh
@@ -10,17 +10,21 @@ if [ $# -ne 0 ]; then
     echo "No arguments neeaded. Ignoring arguments."
 fi
 
+rm -R /usr/local/teamspeak/files/virtualserver_1/channel_1/ts3_bkp_*
+
 filename="ts3_bkp_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz"
 cd /usr/local/teamspeak/
 tar -czvf /root/$filename ts3server.sqlitedb files/
 mv /root/$filename /usr/local/teamspeak/files/virtualserver_1/channel_1/
 chown teamspeak:teamspeak /usr/local/teamspeak/files/virtualserver_1/channel_1/$filename
 
-echo "File $filename is ready to download from teamspeak client default folder."
+echo "File $filename is ready to download from"
+echo "teamspeak client default folder."
 echo "Come back here and type 'ok' when you are done"
 input=""
 while [[ "$input" != "ok" ]]; do
     read $input
 done
+
 rm /usr/local/teamspeak/files/virtualserver_1/channel_1/$filename
 exit
