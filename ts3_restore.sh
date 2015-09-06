0eceeb9c0b7dc0a4eababf6e73a0fb3ad32392d5
Federico Krum <federico@krum.com.ar>
2015-09-06 16:36:49 -0300

    Update ts3_restore.sh

--- ts3_restore.sh
+++ ts3_restore.sh
@@ -21,12 +21,31 @@ fi
     cd /usr/local/teamspeak/files/virtualserver_1/channel_1
     echo "Aca esta!"
     # hacer algo
+    
+prompt="Please select a file:"
+options=( $(find -maxdepth 1 -print0 | xargs -0) )
+
+PS3="$prompt "
+select opt in "${options[@]}" "Quit" ; do 
+    if (( REPLY == 1 + ${#options[@]} )) ; then
+        exit
+
+    elif (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
+        echo  "You picked $opt which is file $REPLY"
+        break
+
+    else
+        echo "Invalid option. Try another one."
+    fi
+done    
+
+ls -ld $opt
+    
+    
     for lines in `ls | grep  ^ts3_bkp_.*\.tar.gz$`
-
-do
-echo $lines
-
-done
+    do
+        echo $lines
+    done
     
     
     #rm /usr/local/teamspeak/files/virtualserver_1/channel_1/$1
