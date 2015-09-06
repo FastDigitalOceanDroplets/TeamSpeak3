2b535b3f1022917a95e9d94e0fde3874a7705d77
Federico Krum <federico@krum.com.ar>
2015-09-06 17:09:36 -0300

    Update ts3_restore.sh

--- ts3_restore.sh
+++ ts3_restore.sh
@@ -22,24 +22,25 @@ fi
     echo "Aca esta!"
     # hacer algo
     
-prompt="Please select a file:"
-options=( $(find -maxdepth 1 -print0 | xargs -0 | sed -r 's/^.{2}//') )
-PS3="$prompt "
-select opt in "${options[@]}" "Quit" ; do 
-    if (( REPLY == 1 + ${#options[@]} )) ; then
-        exit
+    prompt="Please select a file:"
+    options=( $(find -maxdepth 1 -print0 | xargs -0 | sed -r 's/^.{2}//') )
+    PS3="$prompt "
+    select opt in "${options[@]}" "Quit" ; do 
+        if (( REPLY == 1 + ${#options[@]} )) ; then
+            exit
+    
+        elif (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
+            #echo  "You picked $opt which is file $REPLY"
+            break
 
-    elif (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
-        #echo  "You picked $opt which is file $REPLY"
-        break
+        else
+            echo "Invalid option. Try another one."
+        fi
+    done
 
-    else
-        echo "Invalid option. Try another one."
-    fi
-done    
-echo $options
-exit
-
+    service teamspeak stop
+    tar -xzf /usr/local/teamspeak/files/virtualserver_1/channel_1/$opt
+    service teamspeak start
     # rm /usr/local/teamspeak/files/virtualserver_1/channel_1/$opt
     exit
 #fi
