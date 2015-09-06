73a35e3069d7760cc516847496a74f8781cccc3c
Federico Krum <federico@krum.com.ar>
2015-09-06 14:58:40 -0300

    Update ts3_restore.sh

--- ts3_restore.sh
+++ ts3_restore.sh
@@ -18,6 +18,7 @@ fi
 #    echo "Upload file thoug TS3 to the default channel first"
 #    exit 1
 #else
+    cd /usr/local/teamspeak/files/virtualserver_1/channel_1
     echo "Aca esta!"
     # hacer algo
     ls | grep "^ts3_bkp_" 
