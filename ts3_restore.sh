931ed6a75db4b41632a88416419e6e06b16e6cf3
Federico Krum <federico@krum.com.ar>
2015-09-06 17:21:52 -0300

    Update ts3_restore.sh

--- ts3_restore.sh
+++ ts3_restore.sh
@@ -6,21 +6,7 @@ if [ "x$(id -u)" != 'x0' ]; then
     exit 1
 fi
 
-#if [ $# -eq 0 ]
-#  then
-#    echo "No filename provided."
-#    exit 1
-#fi
-
-
-#if [ ! -f /usr/local/teamspeak/files/virtualserver_1/channel_1/$1 ]; then
-#    echo "File not found!"
-#    echo "Upload file thoug TS3 to the default channel first"
-#    exit 1
-#else
     cd /usr/local/teamspeak/files/virtualserver_1/channel_1
-    echo "Aca esta!"
-    # hacer algo
     
     prompt="Please select a file:"
     options=( $(find -maxdepth 1 -print0 | xargs -0 | sed -r 's/^.{2}//') )
