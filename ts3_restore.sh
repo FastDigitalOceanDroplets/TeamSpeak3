fd3433e148c70b8a2ae3b3233049ba1ef0841262
Federico Krum <federico@krum.com.ar>
2015-09-06 12:06:15 -0300

    Update ts3_restore.sh

--- ts3_restore.sh
+++ ts3_restore.sh
@@ -15,10 +15,15 @@ fi
 
 if [ ! -f /usr/local/teamspeak/files/virtualserver_1/channel_1/$1 ]; then
     echo "File not found!"
+    echo "Upload file thoug TS3 to the default channel first"
     exit 1
 else
     echo "Aca esta!"
     # hacer algo
+    
+    
+    
+    
     rm /usr/local/teamspeak/files/virtualserver_1/channel_1/$1
     exit
 fi
