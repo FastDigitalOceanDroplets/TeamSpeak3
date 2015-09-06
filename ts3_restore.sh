cb583d3b017bd5edac610452485a90142b52de71
Federico Krum <federico@krum.com.ar>
2015-09-06 14:56:59 -0300

    Update ts3_restore.sh

--- ts3_restore.sh
+++ ts3_restore.sh
@@ -6,18 +6,18 @@ if [ "x$(id -u)" != 'x0' ]; then
     exit 1
 fi
 
-if [ $# -eq 0 ]
-  then
-    echo "No filename provided."
-    exit 1
-fi
+#if [ $# -eq 0 ]
+#  then
+#    echo "No filename provided."
+#    exit 1
+#fi
 
 
-if [ ! -f /usr/local/teamspeak/files/virtualserver_1/channel_1/$1 ]; then
-    echo "File not found!"
-    echo "Upload file thoug TS3 to the default channel first"
-    exit 1
-else
+#if [ ! -f /usr/local/teamspeak/files/virtualserver_1/channel_1/$1 ]; then
+#    echo "File not found!"
+#    echo "Upload file thoug TS3 to the default channel first"
+#    exit 1
+#else
     echo "Aca esta!"
     # hacer algo
     ls | grep "^ts3_bkp_" 
@@ -26,5 +26,5 @@ else
     
     #rm /usr/local/teamspeak/files/virtualserver_1/channel_1/$1
     exit
-fi
+#fi
 
